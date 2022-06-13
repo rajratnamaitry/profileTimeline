c997ce1d0add057e1aab7590227ed5a4b1c15dcb
rajratnamaitry <rajratnamaitry@gmail.com>
2022-06-13 23:44:13 +0530

    dashboard file updated

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,28 +1,33 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
-import { app, database } from "./firebase";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { app, database } from "../firebase";
+import { useNavigate } from "react-router-dom";
+import { getAuth, onAuthStateChanged,signOut } from "firebase/auth";
 
 export default function Dashboard() {
-    const [isLogin, setIsLogin] = useState(false);
-    const auth = getAuth();
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [isLogin, setIsLogin] = useState(false);
+  useEffect(()=>{
     onAuthStateChanged(auth, (user) => {
       if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/firebase.User
         const uid = user.uid;
         setIsLogin(true);
-        console.log('login', user)
-        // ...
+        console.log('login')
       } else {
-        setIsLogin(false);
         // User is signed out
-        // ...
+        setIsLogin(false);
         console.log('login out')
+        navigate('/login', { replace: true });
       }
     });
+  },[])
+
     // signOut(auth).then(() => {
     //   // Sign-out successful.
+    //   console.log('signout')
     // }).catch((error) => {
     //   // An error happened.
     // });
