d4330287a86abfe56461d2217d6ffa8947dc6dab
rajratnamaitry <rajratnamaitry@gmail.com>
2022-06-18 17:58:48 +0530

    order updated for date

--- src/components/monthTimeLine.jsx
+++ src/components/monthTimeLine.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Badge } from 'react-bootstrap';
 export default function MonthTimeLine({ month, data }) {
-  console.log('month', data[0]);
   return (
     <div className="timeline-section">
       <div className="timeline-date">
@@ -9,7 +8,7 @@ export default function MonthTimeLine({ month, data }) {
         <span>{data.length} Entries</span>
       </div>
       <div className="row">
-        {data.map((project) => (<div className="col-lg-4 col-md-6 col-sm-12 pb-3">
+        {data.sort((a,b)=> b.date.date - a.date.date).map((project) => (<div className="col-lg-4 col-md-6 col-sm-12 pb-3">
           <div className="timeline-box">
             <div className="box-title">
               <i className="fa fa-asterisk text-success" aria-hidden="true"></i> {project.name}
