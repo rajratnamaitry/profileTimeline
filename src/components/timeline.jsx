e6354d069d01463575bdf2b3edbc88e927f3f6fc
rajratnamaitry <rajratnamaitry@gmail.com>
2022-06-18 18:25:45 +0530

    props updated

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -1,11 +1,15 @@
 import React from "react"
 import MonthTimeLine from "./monthTimeLine"
-export default function Timeline({project}) {
+export default function Timeline({project, total}) {
     return (
         <div className="timeline">
             <div className="timeline-month">
                 In { project.year }
-                <span>{ project.nMonthMap.length } Months</span>
+                 {/* total { total } */}
+                <span>{ project.nMonthMap.length } 
+                    {' '} Months
+                    {/* {' '}{ project.totalInMon } */}
+                </span>
             </div>
             {project.nMonthMap.sort((a,b)=>b.mon - a.mon).map(e=><MonthTimeLine key={e.mon} data={e.monData}  month={e.month}/>)}
         </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,6 +5,7 @@ import { app, database } from '../firebase';
 import { getDocs, collection } from 'firebase/firestore'
 export default function Homepage() {
   const [list, setList] = useState([]);
+  const [inYr, setTotalInYr] = useState(0);
   const collectionRef = collection(database, 'projects');
   const getProjectList = () => {
     const monthNames = ["January", "February", "March", "April", "May", "June",
@@ -24,19 +25,24 @@ export default function Homepage() {
       const monthArray = Array(12).fill().map((v,i)=>i);
       const nYear = Array.from(new Set(projectList.map((e)=> e.date.year )));
       const newProjectList = [];
+      let totalInYr = 0;
       nYear.forEach((year)=>{
         const groupYear = projectList.filter((e)=> e.date.year == year );
         const nMonthMap = [];
+        let totalInMon = 0
         monthArray.forEach((mon)=>{ 
           const monData = groupYear.filter(e=> {
            return e.date.mon == mon
           });
           if(monData.length){
+            totalInMon += monData.length;
             nMonthMap.push({mon,monData, month:monthNames[mon]}); 
           }
         });
-        newProjectList.push({year,nMonthMap})
+        totalInYr+=totalInMon;
+        newProjectList.push({year,nMonthMap, inYear: groupYear.length, totalInMon})
       });
+      setTotalInYr(totalInYr);
       setList(newProjectList);
     });
   }
@@ -47,7 +53,7 @@ export default function Homepage() {
   return (
     <div className="container">
       <Header />
-      {list.sort((a,b)=>b.year - a.year).map(e => <TimeLine key={e.id} project={e} />)}
+      {list.sort((a,b)=>b.year - a.year).map(e => <TimeLine key={e.id} project={e} total={inYr} />)}
     </div>
   )
 }
