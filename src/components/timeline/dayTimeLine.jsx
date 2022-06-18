8ab071060f820d1abdb68104445732be0416fb4a
rajratnamaitry <rajratnamaitry@gmail.com>
2022-06-18 23:47:44 +0530

    icon added

--- src/components/timeline/dayTimeLine.jsx
+++ src/components/timeline/dayTimeLine.jsx
@@ -1,5 +1,9 @@
 import { Badge } from 'react-bootstrap';
-export default function DayTimeLine({ project }) {
+import { FaVuejs } from 'react-icons/fa';
+import { DiReact } from 'react-icons/di';
+import { SiAndroid, SiAngular, SiBookstack, SiCss3, SiElectron, SiFirebase, SiGithub, SiHtml5, SiJavascript, SiMedium, SiNextdotjs, SiNginx, SiNpm, SiReact, SiVisualstudiocode } from 'react-icons/si';
+function iconSetting(key) {
+  const SIZE = 20;
   const lang = {
     "Android": 'SiAndroid',
     "Angular": 'SiAngular',
@@ -21,6 +25,46 @@ export default function DayTimeLine({ project }) {
     "VScode": 'SiVisualstudiocode',
     "Vue": 'FaVuejs',
   };
+  switch (key) {
+    case 'Android':
+      return <SiAndroid size={SIZE}/>
+    case 'Angular':
+      return <SiAngular size={SIZE}/>
+    case 'CSS':
+      return <SiCss3 size={SIZE}/>
+    case 'Chrome-ext':
+      return <SiAndroid size={SIZE}/>
+    case 'Electron':
+      return <SiElectron size={SIZE}/>
+    case 'Firebase':
+      return <SiFirebase size={SIZE}/>
+    case 'Github':
+      return <SiGithub size={SIZE}/>
+    case 'HTML':
+      return <SiHtml5 size={SIZE}/>
+    case 'JAVASCRIPT':
+      return <SiJavascript size={SIZE}/>
+    case 'Library':
+      return <SiBookstack size={SIZE}/>
+    case 'Medium.com':
+      return <SiMedium size={SIZE}/>
+    case 'NPM':
+      return <SiNpm size={SIZE}/>
+    case 'Next.js':
+      return <SiNextdotjs size={SIZE}/>
+    case 'Nginx':
+      return <SiNginx size={SIZE}/>
+    case 'React':
+      return <DiReact size={SIZE}/>
+    case 'VScode':
+      return <SiVisualstudiocode size={SIZE}/>
+    case 'Vue':
+      return <FaVuejs size={SIZE}/>
+    default:
+      return '';
+  }
+}
+export default function DayTimeLine({ project }) {
   return (
     <div className="col-lg-4 col-md-6 col-sm-12 pb-3">
       <div className="timeline-box">
@@ -31,7 +75,8 @@ export default function DayTimeLine({ project }) {
           {/* <a className="btn btn-sm btn-outline-light">Details</a> */}
           <div className="box-item"><strong>Tech stack</strong>: {' '}
             {project.stack
-              .map((tech) => <span key={tech + '_' + project.stack}><Badge pill bg="light" text="dark">{tech}</Badge> {' '}</span>)}
+              .map((tech) => <span key={tech + '_' + project.stack}>
+                <Badge pill bg="light" text="dark">{tech}{' '}{iconSetting(tech)}</Badge> {' '}</span>)}
           </div>
           <div className="box-item"><strong>Link </strong>:
             {' '}{project.github ? <a className='text-light' href={project.github} target='_blank' >Github</a> : ''}
