a2ad992977809f2cc8eb6a8e7ee65ad88ba6d90e
rajratnamaitry <rajratnamaitry@gmail.com>
2022-06-18 15:09:51 +0530

    social icon updated

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import { BsLinkedin } from "react-icons/bs";
+import { DiGithubBadge, DiStackoverflow } from "react-icons/di";
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { useNavigate } from "react-router-dom";
+
 import img from '../image.jpg'
 export default function Header() {
+    const SIZE = 30;
+    const CLASS_NAME = "btn btn-dark btn-outline-light btn-borderLess";
+    let navigate = useNavigate();
+    const routeChange = (path) => {
+        navigate(path, { replace: true, state: false });
+    }
     return (
         <div className="header">
             <img src={img} alt="Avatar" className="avatar" />
             <h1>Rajratna Maitry</h1>
+            <ButtonGroup size="sm" >
+                <a className={CLASS_NAME} target="_blank" href="https://github.com/rajratnamaitry/" >
+                    <DiGithubBadge size={SIZE} />
+                </a>
+                <a className={CLASS_NAME} target="_blank" href='https://stackoverflow.com/users/4549472/rajratna-maitry' >
+                    <DiStackoverflow size={SIZE} />
+                </a>
+                <a className={CLASS_NAME} target="_blank" href="https://www.linkedin.com/in/rajratna-maitry-9b7084104/" >
+                    <BsLinkedin size={SIZE} />
+                </a>
+            </ButtonGroup>
             <h2>A timeline that shows open source contributions all the way to present day.</h2>
         </div>
     )
--- src/components/monthTimeLine.jsx
+++ src/components/monthTimeLine.jsx
@@ -1,35 +1,35 @@
 import React from 'react'
 import { Badge } from 'react-bootstrap';
-export default function MonthTimeLine({month, data}) {
-  console.log('month',data[0]);
+export default function MonthTimeLine({ month, data }) {
+  console.log('month', data[0]);
   return (
     <div className="timeline-section">
       <div className="timeline-date">
         In {month}
-        <span>{ data.length } Entries</span>
+        <span>{data.length} Entries</span>
       </div>
       <div className="row">
-        {data.map((project)=>(<div className="col-lg-4 col-md-6 col-sm-12 pb-3">
-            <div className="timeline-box">
-              <div className="box-title">
-                <i className="fa fa-asterisk text-success" aria-hidden="true"></i> {project.name}
+        {data.map((project) => (<div className="col-lg-4 col-md-6 col-sm-12 pb-3">
+          <div className="timeline-box">
+            <div className="box-title">
+              <i className="fa fa-asterisk text-success" aria-hidden="true"></i> {project.name}
+            </div>
+            <div className="box-content">
+              {/* <a className="btn btn-sm btn-outline-light">Details</a> */}
+              <div className="box-item"><strong>Tech stack</strong>: {' '}
+                {project.stack.map((tech) => <><Badge pill bg="light" text="dark">{tech}</Badge> {' '}</>)}
               </div>
-              <div className="box-content">
-                {/* <a className="btn btn-sm btn-outline-light">Details</a> */}
-                <div className="box-item"><strong>Tech stack</strong>: {' '}
-                  {project.stack.map((tech)=><><Badge pill bg="light" text="dark">{tech}</Badge> {' '}</> )}
-                </div>
-                <div className="box-item"><strong>Link </strong>: 
-                  <a className={project.github? 'text-link' : 'text-mute text-black' } href={project.github} target='_blank' >Github</a> ,
-                  <a className={project.url? 'text-link' : 'text-mute text-black' } href={project.url} target='_blank' >Publish</a>
-                </div>
-                <div className="box-item"><strong>Date</strong>: {project.date.displayDate}</div>
+              <div className="box-item"><strong>Link </strong>:
+                {' '}{project.github ? <a className='text-light' href={project.github} target='_blank' >Github</a> : ''}
+                {' '}{project.url ? <a className='text-light' href={project.url} target='_blank' >Public url</a> : ''}
               </div>
-              {/* <div className="box-footer">- Tyler</div> */}
+              <div className="box-item"><strong>Date</strong>: {project.date.displayDate}</div>
+            </div>
+            {/* <div className="box-footer">- Tyler</div> */}
           </div>
         </div>)
         )}
-    </div>
+      </div>
     </div>
   )
 }
